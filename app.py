9f0dfd36fc082ab6da1e1172e6045dc8733b3e36
ny0505 <victoria.c.chang@gmail.com>
2025-10-15 22:51:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -114,13 +114,3 @@ def create_app():
 # WSGI entry point (no __main__)
 
 app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
